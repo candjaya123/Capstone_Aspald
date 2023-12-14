67d0ed68ae82bb89ffbb914ebcd51af02d3ac9d5
egasa21 <esaputra168@gmail.com>
2023-12-14 23:22:54 +0700

    create user models and migrations

---
+++ src/api/v1/models/user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const bcrypt = require('bcrypt')
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+  
+    }
+    async comparePassword(password) {
+      return bcrypt.compare(password, this.password)
+    }
+  }
+
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    address: DataTypes.STRING,
+    dob: DataTypes.DATE,
+    phone: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    hooks: {
+      beforeCreate: async (user) => {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt)
+      }
+    }
+  });
+  return User;
+};
