f8d396a95be9c29c17a26320060b78991bb058d9
egasa21 <esaputra168@gmail.com>
2023-12-15 04:20:31 +0700

    create report model and migration

---
+++ src/api/v1/migrations/20231214211719-create-reports.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reports', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      damageType: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      photoUrl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lat: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      lon: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reports');
+  }
+};
---
+++ src/api/v1/models/reports.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Reports extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Reports.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    damageType: DataTypes.STRING,
+    photoUrl: DataTypes.STRING,
+    lat: DataTypes.FLOAT,
+    lon: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Reports',
+  });
+  return Reports;
+};
