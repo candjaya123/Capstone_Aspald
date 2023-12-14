71cbdf3b972dfa9813162f8907db73363b25b5c5
egasa21 <esaputra168@gmail.com>
2023-12-14 23:03:30 +0700

    FEAT: add error handling

--- src/app.js
+++ src/app.js
@@ -17,6 +17,18 @@ app.get('/', (req, res) =>{
     res.json({ message: "Server On "})
 })
 
+// Error handling
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || 'Something went wrong'
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    });
+});
+
 const syncDb = async () =>{
     try {
         await db.sequelize.sync()
